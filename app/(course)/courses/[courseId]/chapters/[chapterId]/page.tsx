969a395f1a05cb6260ce34d2e78a7d2dcd7b842a
Martin Eduardo Aguayo <martineduardoaguayo@gmail.com>
2024-04-15 13:08:03 -0200

    20: 50% Chaper HLS Video Player

---
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -0,0 +1,49 @@
+import { getChapter } from "@/actions/get-chapter";
+import { Banner } from "@/components/banner";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+const ChapterIdPage = async ({
+  params,
+}: {
+  params: { courseId: string; chapterId: string };
+}) => {
+  const { userId } = auth();
+  if (!userId) return redirect("/");
+
+  const {
+    chapter,
+    course,
+    muxData,
+    attachments,
+    nextChapter,
+    userProgress,
+    purchase,
+  } = await getChapter({
+    userId,
+    chapterId: params.chapterId,
+    courseId: params.courseId,
+  });
+
+  if (!chapter || !course) {
+    return redirect("/");
+  }
+
+  const isLocked = !chapter.isFree && !purchase;
+  const completeOnEnd = !!purchase && !userProgress?.isCompleted;
+
+  return (
+    <div>
+      {userProgress?.isCompleted && (
+        <Banner label={"Ya has completado este Capitulo."} variant="success" />
+      )}
+      {isLocked && (
+        <Banner
+          label={"Necesitas comprar este curso para poder ver el Capitulo."}
+          variant="warning"
+        />
+      )}
+    </div>
+  );
+};
+export default ChapterIdPage;
--- app/(course)/courses/[courseId]/page.tsx
+++ app/(course)/courses/[courseId]/page.tsx
@@ -1,8 +1,29 @@
-const CourseIdPage = () => {
-  return (
-    <div>
-      <h1>Course ID Page</h1>
-    </div>
+import db from "@/lib/db";
+import { redirect } from "next/navigation";
+
+const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
+  const course = await db.course.findUnique({
+    where: {
+      id: params.courseId,
+    },
+    include: {
+      chapters: {
+        where: {
+          isPublished: true,
+        },
+        orderBy: {
+          position: "asc",
+        },
+      },
+    },
+  });
+
+  if (!course) {
+    return redirect("/");
+  }
+
+  return redirect(
+    `/courses/${params.courseId}/chapters/${course.chapters[0].id}`,
   );
 };
 
